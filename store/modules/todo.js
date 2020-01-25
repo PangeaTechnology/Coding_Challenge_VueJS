80df2217596ca7dd85630c804fcb42f1e64f65d8
mohamed khaled <mo7amd.khaled@gmail.com>
2020-01-25 16:27:04 +0200

    add delete and complete actions and mutation

--- store/modules/todo.js
+++ store/modules/todo.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 export const CREATE_ITEM = 'CREATE_ITEM';
 export const READ_ITEMS = 'READ_ITEMS';
 export const UPDATE_ITEM = 'UPDATE_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const COMPLETE_ITEM = 'COMPLETE_ITEM';
 
 const state = {};
 
@@ -33,6 +35,12 @@ const mutations = {
       } 
     }
   },
+  [DELETE_ITEM](state, id) {
+    Vue.delete(state, id);
+  },
+  [COMPLETE_ITEM](state, id) {
+    state[id].complete = true;
+  }
 };
 const actions = {
   [CREATE_ITEM]({ commit }, payload) {
@@ -41,6 +49,12 @@ const actions = {
   [UPDATE_ITEM]({ commit }, payload) {
     commit(UPDATE_ITEM, payload);
   },
+  [DELETE_ITEM]({ commit }, payload) {
+    commit(DELETE_ITEM, payload);
+  },
+  [COMPLETE_ITEM]({ commit }, payload) {
+    commit(COMPLETE_ITEM, payload);
+  }
 };
 const getters = {
   [READ_ITEMS]: state => state,
