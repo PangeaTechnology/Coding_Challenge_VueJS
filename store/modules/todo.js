79c09d291dd2ee7329920998cc4eb0a7fd6ed99f
mohamed khaled <mo7amd.khaled@gmail.com>
2020-01-25 16:26:08 +0200

    add the update action and mutation

--- store/modules/todo.js
+++ store/modules/todo.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 export const CREATE_ITEM = 'CREATE_ITEM';
 export const READ_ITEMS = 'READ_ITEMS';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
 
 const state = {};
 
@@ -15,10 +16,30 @@ const mutations = {
     };
     Vue.set(state, [id], item);
   },
+  [UPDATE_ITEM](state, payload) {
+    const { id, data} = payload;
+    if (typeof data === 'object') {
+      state[id] = {
+        ...state[id],
+        data: {
+          ...state[id].data,
+          ...data,
+        },
+      }
+    } else {
+      state[id] = {
+        ...state[id],
+        data,
+      } 
+    }
+  },
 };
 const actions = {
   [CREATE_ITEM]({ commit }, payload) {
     commit(CREATE_ITEM, payload);
+  },
+  [UPDATE_ITEM]({ commit }, payload) {
+    commit(UPDATE_ITEM, payload);
   },
 };
 const getters = {
