05e10410c59ffe9b8e303230ca64e31be6bb21f6
mohamed khaled <mo7amd.khaled@gmail.com>
2020-01-25 16:17:17 +0200

    create module for the todo and add it to the store

--- store/index.js
+++ store/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { todo } from './modules/todo';
 import LocalStorgeStore from './localStorgePlugin';
 
 Vue.use(Vuex)
 const isProd = process.env.NODE_ENV === 'production';
 
 export default new Vuex.Store({
-  
+  modules: {
+    todo,
+  },
   mutations: {
     initStore() {
       if(localStorage.getItem('App')) {
---
+++ store/modules/todo.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+
+
+const state = {};
+
+const mutations = {
+  
+};
+const actions = {
+  
+};
+const getters = {
+  
+};
+
+export const todo = {
+  state,
+  getters,
+  actions,
+  mutations,
+};
