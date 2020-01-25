cadfbc81b4e5184344072da6498c417fc8312103
mohamed khaled <mo7amd.khaled@gmail.com>
2020-01-25 16:19:17 +0200

    itegrate the create action with todo form

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -3,11 +3,42 @@
     label="Todo"
     placeholder="What do you have to do?"
     solo
+    clearable
+    v-model="updatedValue"
+    @keydown="onkeyDownHandler"
   />
 </template>
 
 <script>
+import { CREATE_ITEM } from '../../store/modules/todo';
+
 export default {
+  data: function() {
+    return {
+      updatedValue: '',
+    };
+  },
+  methods: {
+    addItem() {
+      const payload = this.updatedValue;
+      this.$store.dispatch(CREATE_ITEM, payload)
+        .then(() => {
+          this.updatedValue = '';
+        })
+    },
+    onkeyDownHandler(e) {
+      const {
+        keyCode,
+        key,
+        isTrusted,
+        } = e;
+      const isEnter = key === 'Enter' || keyCode === 13;
+      const isGenuinEnter = isTrusted && isEnter;
+      if (isGenuinEnter) {
+        this.addItem();
+      }
+    },
+  }
 }
 </script>
 
