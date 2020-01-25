39ca40281a391b5d2e9d156a363d79dd5f42fadf
mohamed khaled <mo7amd.khaled@gmail.com>
2020-01-25 16:18:51 +0200

    add create item action and mutation

--- store/modules/todo.js
+++ store/modules/todo.js
@@ -1,16 +1,26 @@
 import Vue from 'vue'
 
+export const CREATE_ITEM = 'CREATE_ITEM';
 
 const state = {};
 
 const mutations = {
-  
+  [CREATE_ITEM](state, data) {
+    const id = new Date().getTime();
+    const item = {
+      id,
+      complete: false,
+      data: data,
+    };
+    Vue.set(state, [id], item);
+  },
 };
 const actions = {
-  
+  [CREATE_ITEM]({ commit }, payload) {
+    commit(CREATE_ITEM, payload);
+  },
 };
 const getters = {
-  
 };
 
 export const todo = {
