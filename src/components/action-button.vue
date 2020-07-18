dd9ae68e39d8cadcf33760bf77aea95d20e4dd94
Muhammad Ibrahim <engineermohamed.ibrahim.mi@gmail.com>
2020-07-18 13:56:22 +0200

    capsulated v-btn component into ActionButton for reusability

---
+++ src/components/action-button.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <v-btn
+    :color="color"
+    @click="$emit('callback')"
+  >
+    {{ says }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: ''
+    },
+    says: {
+      type: String,
+      default: ''
+    },
+    callback: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
