e0683d76f52b42fbea6b142c208dd2ab7c3a7325
Muhammad Ibrahim <engineermohamed.ibrahim.mi@gmail.com>
2020-07-18 14:03:09 +0200

    applied crud operations

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -1,15 +1,44 @@
 <template lang="html">
-  <v-text-field
-    label="Todo"
-    placeholder="What do you have to do?"
-    solo
-  />
+  <div class="add-todo">
+    <v-text-field
+      label="Todo"
+      placeholder="What do you have to do?"
+      v-model="todo"
+      @keyup.enter="addTodo()"
+      solo
+    />
+    <action-button
+      says="add"
+      color="success"
+      @callback="addTodo()"
+    />
+  </div>
 </template>
 
 <script>
+import ActionButton from '@/components/action-button'
+import store from '@/store'
+
 export default {
+  components: {
+    ActionButton
+  },
+  data: () => {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    addTodo: function() {
+      store.dispatch('addTodo', this.todo)
+      this.todo = ''
+    }
+  }
 }
 </script>
 
 <style lang="css">
+.add-todo {
+  display: flex;
+}
 </style>
--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -1,23 +1,59 @@
 <template lang="html">
-  <v-card>
-    <v-card-title primary-title>
-      <div>
-        <h3 class="headline mb-0">
-          Title
-        </h3>
-      </div>
-    </v-card-title>
-    <v-btn>
-      Complete
-    </v-btn>
-    <v-btn color="error">
-      Delete
-    </v-btn>
-  </v-card>
+  <div>
+    <v-card>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">
+            {{ todo.title }}
+          </h3>
+        </div>
+      </v-card-title>
+      <action-button
+        :says="todo.done ? 'Re-activate' : 'Complete'"
+        @callback="toggleCompleteCb(todo)"
+      />
+      <action-button
+        color="error"
+        says="Delete"
+        @callback="deleteCb(todo)"
+      />
+    </v-card>
+  </div>
 </template>
 
 <script>
+
+import ActionButton from '@/components/action-button'
+import store from '@/store';
+
 export default {
+  components: {
+    ActionButton
+  },
+  props: {
+    todo: {
+      type: Object,
+      default: () => {
+        return {title: '', done: false}
+      }
+    },
+    index: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: () => {
+    return {
+    }
+  },
+  methods: {
+    toggleCompleteCb: todo => {
+      store.commit('toggleCompleteTodo', todo)
+    },
+    deleteCb: todo => {
+      store.commit('deleteTodo', todo)
+    }
+  }
 }
 </script>
 
